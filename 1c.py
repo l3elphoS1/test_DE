05f09b02cf39d56580267b1584b8591321e19c93
l3elphoS1 <110003636+l3elphoS1@users.noreply.github.com>
2024-11-16 11:57:57 +0700

    update

--- 1c.py
+++ 1c.py
@@ -1,3 +1,21 @@
+import pandas as pd 
+from datetime import datetime
+
+file_path = "./Test - input-applicant.xlsx"
+excel = pd.ExcelFile(file_path)
+
+ex1 = pd.read_excel(excel,sheet_name='transactions')
+ex2 =  pd.read_excel(excel,sheet_name='UserName')
+ex3 =  pd.read_excel(excel,sheet_name='Name')
+ex4 =  pd.read_excel(excel,sheet_name='Output ')
+
+transactions = pd.DataFrame(ex1)
+username = pd.DataFrame(ex2)
+name = pd.DataFrame(ex3)
+output = pd.DataFrame(ex4)
+
+
+
 # Convert Buddhist Era (BE) to Gregorian (AD)
 def be_to_ad(be_date_str):
     # Parse the date in Buddhist Era format
